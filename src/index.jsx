c47a3142b9e5d3d65d757116997538b71464a5f2
Riza Fahmi <rizafahmi@gmail.com>
2017-01-07 11:47:10 +0700

    Add forgotten ,

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,11 @@ import {List, Map} from 'immutable'
 import ToDoneApp from './components/ToDoneApp'
 
 const todone = List.of(
-  Map({id: 1, text: 'React', status: 'active', editing: false})
-  Map({id: 2, text: 'Redux', status: 'active', editing: false})
+  Map({id: 1, text: 'React', status: 'active', editing: false}),
+  Map({id: 2, text: 'Redux', status: 'active', editing: false}),
   Map({id: 3, text: 'Enzyme', status: 'completed', editing: false})
 )
 
 ReactDOM.render(
-  <ToDoneApp todos={todos} />, document.getElementById('app')
+  <ToDoneApp todos={todone} />, document.getElementById('app')
 )
